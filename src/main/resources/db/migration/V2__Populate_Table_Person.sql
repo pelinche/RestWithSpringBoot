0e714f760917c0f769cb5f72277b373287be3613
Luis Carlos Eich <pelinche@gmail.com>
2021-01-15 19:59:42 -0300

    Implementes migration on Mysql Database

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(2, 'Itapiranga', 'Outro', 'Male', 'Eich'),
+	(4, 'Itapiranga', 'Outro', 'Male', 'Eich'),
+	(5, 'Itapiranga', 'Luis Carlos', 'Male', 'Eich'),
+	(6, 'Itapiranga', 'Luis Carlos', 'Male', 'Eich'),
+	(9, 'Itapiranga', 'Outro', 'Male', 'Eich'),
+	(10, 'Itapiranga', 'Luis Carlos', 'Male', 'Eich');
